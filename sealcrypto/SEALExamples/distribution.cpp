a8890a781a4876302ea27513d5271d98d9cb25a6
MaevaB <maevabenoit@yahoo.fr>
2016-07-08 14:20:20 +0200

    New version of the simulation (still in progess)

---
+++ sealcrypto/SEALExamples/distribution.cpp
@@ -0,0 +1,222 @@
+#include <vector>
+#include <unistd.h>
+#include <ctime>
+#include <cstring>
+#include <cmath>
+#include <fstream>
+#include <ctime>
+
+#include <stdexcept>
+
+#include "seal.h"
+
+#ifndef CLEAR
+
+#define CLEAR "clear"
+
+#endif
+
+//binary size of label (must be even)
+#define SIZE_LABEL 6
+
+//upper bound on the values associated with each label
+#define BOUND_VALUE 50
+
+//number of users
+#define NB_USERS 3
+
+//base for encoding
+#define BASE 3
+
+using namespace std;
+using namespace seal;
+
+void print_example_banner(string title);
+void intToTabBin(unsigned int num, int **res, int size_label);
+
+void custom_parameters(EncryptionParameters &parms, int size_label, int nb_users, int bound_value);
+ChooserPoly get_label(ChooserEncoder &encoder, ChooserEncryptor &encryptor,ChooserEvaluator &evaluator, ChooserPoly elem_label, int index_start, int index_end);
+ChooserPoly equals(ChooserEncoder &encoder, ChooserEvaluator &evaluator, ChooserPoly elem_label);
+ChooserPoly copyPoly(ChooserPoly poly);
+
+int main(int argc, char *argv[]){
+
+  int size_label=0;
+
+  //A changer plus tard
+  int nb_users=NB_USERS;
+  int bound_value = BOUND_VALUE;
+  int base = BASE;
+
+  if (argc > 2){
+    cout << "Invalid argument" << endl;
+    cout << "Usage : ./distribution.exe <size_entry>" << endl;
+    return 1;
+  }
+
+  if (argc == 1){
+    size_label=SIZE_LABEL;
+    cout << "Default parameters : size = " << size_label << endl;
+  }
+  
+  if (argc == 2){
+    size_label = atoi(argv[1]);
+    cout << "User parameters : size = " << size_label << endl;
+  }
+
+
+  print_example_banner("Distribution");
+
+  cout << "Running example for " << nb_users << " users, " << size_label << " bits labels and values of max size " << bound_value << endl;
+
+  //Initialize parameters
+  cout << "Initializing encryption parameters..." << endl;
+
+  //Encryption parameters
+  EncryptionParameters parms;
+
+  //Custom encryption parameters
+  custom_parameters(parms, size_label, nb_users, bound_value);
+
+    cout << "DONE" << endl;
+
+  cout << "Encryption parameters specify " << parms.poly_modulus().significant_coeff_count() << " coefficients with "
+       << parms.coeff_modulus().significant_bit_count() << " bits per coefficient" << endl;
+  cout << "Plain modulus : " << parms.plain_modulus().to_string() << endl;
+
+
+  /*  if (parms.plain_modulus() < (BASE + 1)/2){
+    parms.plain_modulus() = (BASE + 1)/2;
+    cout << "new plain modulus : " << parms.plain_modulus().to_string() << endl;
+  }
+  */
+
+  //parms.plain_modulus() = 1 << ( (int)ceil(log2(bound_value)) );
+
+  //Encoder
+  BalancedEncoder encoder(parms.plain_modulus());
+
+  //Generate keys
+  cout << "Generating keys..." << endl;
+  KeyGenerator generator(parms);
+  generator.generate();
+  cout << "... key generation complete" << endl;
+  BigPolyArray public_key = generator.public_key();
+  BigPoly secret_key = generator.secret_key();
+
+  //Encryptor
+  Encryptor encryptor(parms, public_key);
+
+  //Evaluator
+  Evaluator evaluator(parms);
+
+  //Decryptor
+  Decryptor decryptor(parms, secret_key);
+
+
+  return 0;
+  }
+
+void custom_parameters(EncryptionParameters &parms, int size_label, int nb_users, int bound_value){
+  cout << "custom parameters (this may take some time)" << endl;
+  ChooserEncoder encoder;
+  ChooserEncryptor encryptor;
+  ChooserEvaluator evaluator;
+
+
+  //elem_label represents one elem  of the binary decomp of the label
+  ChooserPoly elem_label(1,1);
+
+  ChooserPoly result;
+
+  //Pour encoding en base 3
+  ChooserPoly value(ceil(log(bound_value)/log(3)), 1);//ou different second terme ??
+
+
+  cout << "calcul unique" << endl;
+  
+  //when we put 0 for the forst term of the mult, it won't give accurate plain_modulus != when we use 1...
+  ChooserPoly mult = evaluator.multiply(get_label(encoder, encryptor, evaluator, elem_label, 0, size_label - 1), value);
+
+  result = copyPoly(mult);
+  ChooserPoly cp = copyPoly(mult);
+
+  cout << "add copies" << endl;
+
+  for (int i = 1; i < (1<<size_label)*nb_users; i++){
+    result = evaluator.add(cp, result);
+    }
+
+  cout << "select parameters" << endl;
+  bool select_ok = evaluator.select_parameters(result, parms);
+  cout << "Select return value  : " << select_ok << endl;
+}
+
+//data contains size_bin encrypted bits of label without the encrypted value of data (not useful for noise growth)
+//Here we want to check whether the encrypted label is the same as the one in tab_bin_label
+ChooserPoly get_label(ChooserEncoder &encoder, ChooserEncryptor &encryptor,ChooserEvaluator &evaluator, ChooserPoly elem_label, int index_start, int index_end){
+
+  if (index_end == index_start){
+    return equals(encoder, evaluator, elem_label);
+
+  }
+  else if (index_end - index_start == 1){
+    return evaluator.multiply(equals(encoder, evaluator, elem_label), equals(encoder, evaluator, elem_label));
+  }
+  
+  else{
+    return evaluator.multiply(get_label(encoder, encryptor, evaluator, elem_label, index_start, floor((index_start+index_end)/2)), get_label(encoder, encryptor, evaluator, elem_label, floor((index_start+index_end)/2)+1, index_end));
+  }
+
+}
+
+//"returns an encrypted value of one if the poly p encrypts i, and an encrypted value of zero if not."
+ChooserPoly equals(ChooserEncoder &encoder, ChooserEvaluator &evaluator, ChooserPoly elem_label){
+
+  return evaluator.add_plain(evaluator.negate(elem_label), encoder.encode(1));
+  //more noise when we want to match zero so let's always compute this case.
+}
+
+ChooserPoly copyPoly(ChooserPoly poly){
+  ChooserPoly P(poly);
+  return P;
+}
+
+void print_example_banner(string title)
+{
+  if (!title.empty())
+    {
+      size_t title_length = title.length();
+      size_t banner_length = title_length + 2 + 2 * 10;
+      string banner_top(banner_length, '*');
+      string banner_middle = string(10, '*') + " " + title + " " + string(10, '*');
+
+      cout << endl
+	   << banner_top << endl
+	   << banner_middle << endl
+	   << banner_top << endl
+	   << endl;
+    }
+}
+
+void intToTabBin(unsigned int num, int **res, int size_label) {
+  // At the end of this function, "res" contains a table of the bit decomposion of "num" (e.g. : res = [0,1,0,0,1,0] for num = 10 with MAX_BITS = 6)
+  if (num > (1 << SIZE_LABEL)) {
+    cout << "Number is bigger than the maximum" << endl;
+    return;
+    }
+
+  *res = (int *) (malloc(size_label * sizeof(int)));
+
+  for (int i=0; i<size_label; i++) {
+    (*res)[SIZE_LABEL-i-1] = (num & (1 << i)) ? 1 : 0;
+  }
+}
+
+/*void encrypt_binary_decomp(vector <int> &tab(SIZE_LABEL), vector <BigPolyArray> & encrypt_tab(SIZE_LABEL), Encryptor encryptor, Encoder encoder){
+
+  for(int i=0; i<SIZE_LABEL; i++){
+    encrypt_tab.at(i) =  encryptor.encrypt(encoder.encode(tab.at(i)));
+  }
+}
+*/
