f48d53bdcaa6ed572f365c61770af48a365d81ef
Lounes <tiouchichinelounes@outlook.com>
2016-07-13 14:32:37 +0200

    adding second method encrypting zeros

--- sealcrypto/distribution/enc_zeros.cpp
+++ sealcrypto/distribution/enc_zeros.cpp
@@ -33,17 +33,6 @@
 using namespace std;
 using namespace seal;
 
-void intToTabBin(unsigned int num, int *res, int size_label) {
-  // At the end of this function, "res" contains a table of the bit decomposion of "num" (e.g. : res = [0,1,0,0,1,0] for num = 10 with size_label = 6)
-  if ((int) num > (1 << size_label)) {
-    cout << "Number is bigger than the maximum" << endl;
-    return;
-  }
-
-  for (int i=0; i<size_label; i++) {
-    res[size_label-i-1] = (num & (1 << i)) ? 1 : 0;
-  }
-}
 
 void dispTab(int tab[], int len) {
   cout << "[";
@@ -85,32 +74,24 @@ void print_example_banner(string title)
 }
 
 
-//"returns an encrypted value of one if the poly p encrypts i, and an encrypted value of zero if not."
-ChooserPoly equals(ChooserEncoder &encoder, ChooserEvaluator &evaluator, ChooserPoly elem_label){
 
-  return evaluator.add_plain(evaluator.negate(elem_label), encoder.encode(1));
-  //more noise when we want to match zero so let's always compute this case.
+// THis fucntion returns a sommation of all the values related to a label passed as parameter
+BigPolyArray stat2 (vector< vector < BigPolyArray > > &users, Evaluator &evaluator, unsigned int label){
+	
+	BigPolyArray tmp;
+	
+	// We calculate the binary representation of the label
+	//users.size --> NB_USERS ?
+	for (size_t i=0; i < users.size(); ++i){
+		if (i==0) tmp = users.at(i).at(label);
+		else 
+		{
+			tmp = evaluator.add(tmp, users.at(i).at(label)); 
+		}
+	}
+	// We return the sum of all the values of tmp_values
+	return tmp;
 }
-
-
-//data contains size_bin encrypted bits of label without the encrypted value of data (not useful for noise growth)
-//Here we want to check whether the encrypted label is the same as the one in tab_bin_label
-ChooserPoly get_label(ChooserEncoder &encoder, ChooserEncryptor &encryptor,ChooserEvaluator &evaluator, ChooserPoly elem_label, int index_start, int index_end){
-
-  if (index_end == index_start){
-    return equals(encoder, evaluator, elem_label);
-
-  }
-  else if (index_end - index_start == 1){
-    return evaluator.multiply(equals(encoder, evaluator, elem_label), equals(encoder, evaluator, elem_label));
-  }
-  
-  else{
-    return evaluator.multiply(get_label(encoder, encryptor, evaluator, elem_label, index_start, floor((index_start+index_end)/2)), get_label(encoder, encryptor, evaluator, elem_label, floor((index_start+index_end)/2)+1, index_end));
-  }
-
-}
-
 
 void custom_parameters(EncryptionParameters &parms, int size_label, int nb_users, int bound_value, int base){
   //cout << "Custom parameters" << endl;
@@ -123,24 +104,23 @@ void custom_parameters(EncryptionParameters &parms, int size_label, int nb_users
   ChooserEvaluator evaluator;
 
   //size of one coeff in the chosen basis
-  int size_coeff = (base - 1)/2;
-
-  //elem_label represents one element  of the binary decomp of the label
   ChooserPoly result;
 
-  //Pour encoding en base 3
+  //value associated with each label
   ChooserPoly value(ceil(log(bound_value)/log(base)), base);//ou different second terme ??
 
 
-  //when we put 0 for the first term of the mult, it won't give accurate plain_modulus != when we use 1...
-  ChooserPoly mult = evaluator.multiply(get_label(encoder, encryptor, evaluator, elem_label, 0, size_label - 1), value);
+  /* We don't need to consider the labels for the computation since they are not encrypted.
+     We just access the label we want for each user and then add all the data.
+     -> Sum of nb_users terms 
+  */
 
-  result = copyPoly(mult);
-  ChooserPoly cp = copyPoly(mult);
+  result = copyPoly(encryptor.encrypt(value));
+  ChooserPoly cp = copyPoly(encryptor.encrypt(value));
 
   //cout << "add copies" << endl;
 
-  for (int i = 1; i < (1<<size_label)*nb_users; i++){
+  for (int i = 1; i < (nb_users<<1); i++){
     result = evaluator.add(cp, result);
     }
 
@@ -148,9 +128,8 @@ void custom_parameters(EncryptionParameters &parms, int size_label, int nb_users
   dispTime(start_custom);
   clock_t start_select = clock();
 
-  //cout << "select parameters" << endl;
   bool select_ok = evaluator.select_parameters(result, parms);
-  //cout << "Select return value  : " << select_ok << endl;
+
 
   if ( !select_ok){
     cout << endl << "Time spent by the library to select accurate parameters : " << endl;
@@ -163,53 +142,6 @@ void custom_parameters(EncryptionParameters &parms, int size_label, int nb_users
   cout << endl << "Time spent by the library to select accurate parameters : " << endl;
   dispTime(start_select);
   
-}
-
-BigPolyArray match_func (Evaluator &evaluator, vector <BigPolyArray> &label, int beg, int end){
-	if (end == beg) return label[beg];
-	else if (end-beg == 1) return evaluator.multiply(label[beg], label[end]);
-	else return evaluator.multiply(match_func(evaluator, label, beg, (end+beg)/2), match_func(evaluator, label, (end+beg)/2 +1, end));
-}
-
-
-// INDICATIVE FUNCTION
-//int bin_decom_label[size_label]
-BigPolyArray ind_function( Encryptor &encryptor, BalancedEncoder &encoder, Evaluator &evaluator, int *bin_decomp_label, vector <BigPolyArray> &user, BigPolyArray &one, int size_label){
-
-	vector <BigPolyArray> encrypted_label_ind_func(size_label);
-	
-	// We apply the indicative functions for each one of the bits of the label binary decomposition
-	for (int i=0; i<size_label; ++i){
-		if (bin_decomp_label[i] == 0){
-			encrypted_label_ind_func[i] = evaluator.sub(one, user[i])	;
-		}else{
-			encrypted_label_ind_func[i] =  user[i];
-		}
-	}
-	return match_func(evaluator, encrypted_label_ind_func, 0, size_label-1);
-}
-
-
-// THis fucntion returns a sommation of all the values related to a label passed as parameter
-BigPolyArray stat (vector< vector < vector <BigPolyArray>>> &users, Encryptor &encryptor, BalancedEncoder &encoder, Evaluator &evaluator, unsigned int label, BigPolyArray &one, int size_label){
-	vector < BigPolyArray > tmp_values;
-	BigPolyArray tmp;
-	int *bin_decomp = (int *)malloc(sizeof(int) * size_label) ;
-	
-	// We calculate the binary representation of the label 
-	intToTabBin( label, bin_decomp, size_label);
-	cout << "représentation binaire du label : ";
-	dispTab(bin_decomp, size_label);
-
-	// We apply indicative functions for the encrypted table
-
-	for (size_t i=0; i < users.size(); ++i){
-		for (size_t j=0; j<users.at(i).size(); ++j){ 
-			tmp_values.push_back(evaluator.multiply( ind_function(encryptor, encoder, evaluator, bin_decomp, users.at(i).at(j), one, size_label), users.at(i).at(j)[size_label]));
-		}
-	}
-	// We return the sum of all the values of tmp_values
-	return evaluator.add_many(tmp_values);
 }
 
 
@@ -233,7 +165,7 @@ int main(int argc, char *argv[]){
     bound_value = atoi(argv[3]);
     base = atoi(argv[4]);
    
-    if (base%2 == 0){
+    if ( (base%2 == 0) || (base < 3) ){
       cout << "base must be an odd integer at least 3" << endl;
       return (1);
     }
@@ -281,7 +213,6 @@ int main(int argc, char *argv[]){
        << parms.coeff_modulus().significant_bit_count() << " bits per coefficient" << endl;
   cout << "Plain modulus : " << parms.plain_modulus().to_string() << endl << endl;
 
-
   //Encoder
   BalancedEncoder encoder(parms.plain_modulus(), base);
 
@@ -311,25 +242,10 @@ int main(int argc, char *argv[]){
 
   print_example_banner("Actual computation");
 
+//Beginning of init params for test
+  vector<vector<BigPolyArray> > dataBase;
+  vector <BigPolyArray> user;
 
-  BigPolyArray one = encryptor.encrypt(encoder.encode(1));
-
-
-  //Beginning of init params for test
-  vector<vector<vector<BigPolyArray> > > dataBase;
-  vector<vector<BigPolyArray> > user;
-
-  /* 
-     Each row of the user dataBase is as folows :
-
-     [E(b_n), E(b_n-1),..., E(b_1), E(b_0), E(v)]
-
-     Where E(b_a) is the encrypted value of the a-th bit of the bit decomposition of the label (which is an integer)
-     And E(v) is the encrypted value of the corresponding value
-
-     e.g. : [E(0), E(1), E(0), E(1), E(34)] would correspond to the entry "5:34" where 5 would be "taking the bus" for example
-  */
-  vector <BigPolyArray> tmpRow(size_label + 1);
   
   clock_t start_database = clock();
 
@@ -337,29 +253,14 @@ int main(int argc, char *argv[]){
 
   cout << "Generating a random " << nb_users << " users long database..." << endl;
 
-  int *tmpBitDecomp = (int*)malloc(sizeof(int) * size_label) ;
-  int tmpRand;
   cout << "users list generation ... " << endl; 
 
-  for (int i=0; i<nb_users; i++) {
-    for (int j=0; j< (1<<size_label); j++) {
-      // We do not add the null values to the database (which is the whole point of this method)
-      tmpRand = rand() % bound_value;
-      if (tmpRand == 0)	continue;
-      // We take the bit decomposition of the label
-      intToTabBin(j, tmpBitDecomp, size_label);
-    
-      for (int k=0; k<size_label; k++) {
-	// We add the encrypted version of the table in the result vector
-	tmpRow[k] = encryptor.encrypt(encoder.encode(tmpBitDecomp[k]));
-      }
-      // Finally, we add the encrypted value at the end
-      tmpRow[size_label] = (encryptor.encrypt(encoder.encode(tmpRand)));
-      user.push_back(tmpRow);
-     
-    }
-    dataBase.push_back(user);
-    user.clear();
+  for (int i=0; i<nb_users; i++){
+  	for (int j=0; j < (1 << size_label); j++){
+  		user.push_back(encryptor.encrypt(encoder.encode(rand()%bound_value)));
+  	}
+  	dataBase.push_back(user);
+  	user.clear();
   }
   cout << "...generation complete." << endl << endl;;
   cout << "Time for generating database : " << endl;
@@ -367,8 +268,8 @@ int main(int argc, char *argv[]){
 
 
   clock_t computation = clock();
-  BigPolyArray stat_result = stat(dataBase, encryptor, encoder, evaluator, 5, one, size_label);
-	
+  BigPolyArray stat_result = stat2(dataBase, evaluator, 5);
+
   cout << endl<< "label 5 : " << encoder.decode_int64(decryptor.decrypt(stat_result)) << endl;
 
   cout << endl << "Time spent on computation to find the data we want :" << endl;
